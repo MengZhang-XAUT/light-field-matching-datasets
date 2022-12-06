773bace61e3afb1519484c83e4ca58ce2018c533
MengZhang-XAUT <39645192+MengZhang-XAUT@users.noreply.github.com>
2022-12-06 16:04:50 +0800

    Delete FDL-HCGH directory

--- FDL-HCGH/gray_remapping.m
+++
@@ -1,8 +0,0 @@
-function DIS= gray_remapping(iFDL)
-% 将iFDL进行灰度变换
-parfor i=1:size(iFDL,4)    % 傅里叶视差层的层数
-    img=iFDL(:,:,:,i);
-    iimg=img(16:end-15,16:end-15,:);
-    img1=iimg/max(iimg(:))*1;
-    DIS(:,:,:,i)=img1;
-end
